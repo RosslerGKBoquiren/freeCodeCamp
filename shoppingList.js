b4d90b904d4fae53d53bc689ff69d44a1102526e
RosslerGKBoquiren <142700435+RosslerGKBoquiren@users.noreply.github.com>
2023-11-02 23:23:19 -0400

    Add files via upload

---
+++ shoppingList.js
@@ -0,0 +1,12 @@
+// myList should be an array.
+// The first elements in each of your sub-arrays should all be strings.
+// The second elements in each of your sub-arrays should all be numbers.
+// You should have at least 5 items in your list.
+
+const myList = [
+    ["Milk", 5, "Carrot", "Steak", "Chicken"],
+    ["23", 24, 8, 32, 30],
+    ["John", 18, true, "nomad"],
+    ["Jane", 28, false, "simpleton"],
+    ["Jack", 21, false, "of-all-trades"]
+  ];
